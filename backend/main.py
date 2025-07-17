c79bdf1324fdee11d85b9ba5b78b20e925445082
Akhil <159040732+Akhilvallala2023@users.noreply.github.com>
2025-07-17 09:35:17 -0400

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -61,7 +61,7 @@ app = FastAPI(
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173", "http://localhost:5174", "http://127.0.0.1:5174"],
+    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173", "http://localhost:5174", "http://127.0.0.1:5174","https://idyllic-sorbet-caaa3d.netlify.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
